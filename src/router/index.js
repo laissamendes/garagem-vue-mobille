7ee118ee83e0d36739ce28f2c6aeb4bdf3c329f7
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-07-23 11:25:21 -0300

    Criação do projeto para mobille e junção com o backend

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import LayoutFull from '@/layouts/LayoutFull.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: '',
+      component: LayoutFull,
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: HomeView,
+        },
+        {
+          path: '/produtos/adicionar',
+          name: 'ProductAdd',
+          component: () => import('@/views/ProductAdd.vue'),
+        },
+        {
+          path: '/login',
+          name: 'Login',
+          component: () => import('@/views/LoginView.vue'),
+        },
+      ],
+    },
+  ],
+});
+
+export default router;
+
---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import '@passageidentity/passage-elements/passage-auth';
+</script>
+
+<template>
+  <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth
+      app-id="nHPtpWAptJs7cieqisg5YES5"
+    ></passage-auth>
+  </div>
+</template>
+
+<style></style>
